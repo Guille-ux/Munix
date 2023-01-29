ff432466c34b37e54953ab3044045bb3b2df3894
Guille-ux <104579306+Guille-ux@users.noreply.github.com>
2023-01-29 12:41:23 +0000

    Update string.h

--- root/kernel/string.h
+++ root/kernel/string.h
@@ -8,3 +8,16 @@ int strcmp(const char *str1, const char *str2) {
   }
   return str1[i] - str2[i];
 }
+
+char *strncat(char *dest, const char *src, size_t n) {
+size_t dest_len = strlen(dest);
+size_t i;
+
+for (i = 0; i < n && src[i] != '\0'; i++) {
+dest[dest_len + i] = src[i];
+}
+dest[dest_len + i] = '\0';
+
+return dest;
+}
+
