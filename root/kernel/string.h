052a2f77cdc373f41feaee1122427588bb81c9c0
Guille-ux <104579306+Guille-ux@users.noreply.github.com>
2023-01-29 12:39:12 +0000

    Create string.h
    have the function strcmp

---
+++ root/kernel/string.h
@@ -0,0 +1,10 @@
+int strcmp(const char *str1, const char *str2) {
+  int i = 0;
+  while (str1[i] != '\0' && str2[i] != '\0') {
+    if (str1[i] != str2[i]) {
+      return str1[i] - str2[i];
+    }
+    i++;
+  }
+  return str1[i] - str2[i];
+}
