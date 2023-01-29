c6047fad4e8474e1473f687161062a952f731e60
Guille-ux <104579306+Guille-ux@users.noreply.github.com>
2023-01-29 12:43:13 +0000

    Update nsl.h

--- root/kernel/nsl.h
+++ root/kernel/nsl.h
@@ -1,45 +1,36 @@
 #include "keyboard.h"
 #include "print.h"
 #include "string.h"
-#define beff
-#define boff
 
-extern boff;
-extern beff;
+char beff[100];
+char boff[100];
 
 int shell()
 {
-  while (1) {
-    beff = Nkhandler();
-    if (beff != '\n') {
-      boff += beff;
-    } else {
-      if (strcmp(boff, "exit") == 0) {
-        break;
-      } else if (strcmp(boff, "asm") == 0) {
-        asmshell();
-      } else {
-        char *str = "[!] Error: command not found [!]";
-        print(str);
-      }
-      boff = "";
-    }
-  }
+while (strcmp(boff, "exit") != 0) {
+strncat(beff, Nkhandler(), 1);
+if (strcmp(beff, "\n") != 0) {
+strcat(boff, beff);
+} else {
+if (strcmp(boff, "asm") == 0) {
+asmshell();
+} else {
+char *str = "[!] Error: command not found [!]";
+print(str);
+}
+}
+}
+return 0;
 }
 
 int asmshell()
 {
-  while (1) {
-    beff = Nkhandler();
-    if (beff != '\n') {
-      boff += beff;
-    } else {
-      if (strcmp(boff, "exit") == 0) {
-        break;
-      } else {
-        asm volatile(boff);
-      }
-      boff = "";
-    }
-  }
+while (strcmp(boff, "exit") != 0) {
+if (strcmp(boff, "exit") != 0) {
+asm volatile(boff);
+} else {
+shell();
+}
+}
+return 0;
 }
