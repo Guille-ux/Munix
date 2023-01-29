c0353172d6dcba3efbcfd401a78e1abdfb042052
Guille-ux <104579306+Guille-ux@users.noreply.github.com>
2023-01-29 12:38:34 +0000

    Update nsl.h

--- root/kernel/nsl.h
+++ root/kernel/nsl.h
@@ -1,42 +1,45 @@
 #include "keyboard.h"
 #include "print.h"
+#include "string.h"
 #define beff
 #define boff
+
 extern boff;
 extern beff;
+
 int shell()
 {
-  while (boff != "exit"){
+  while (1) {
     beff = Nkhandler();
-    if (beff != "\n")
-    {
+    if (beff != '\n') {
       boff += beff;
-     }
-   else
-    {
-      if boff == "asm"
-      {
-       asmshell();
+    } else {
+      if (strcmp(boff, "exit") == 0) {
+        break;
+      } else if (strcmp(boff, "asm") == 0) {
+        asmshell();
+      } else {
+        char *str = "[!] Error: command not found [!]";
+        print(str);
       }
-      else
-      {
-      char *str = "[!] Error: command not found [!]";
-      print(str);
-      }
+      boff = "";
     }
   }
 }
+
 int asmshell()
 {
-  while (boff != "exit")
-  {
-    if (boff != "exit")
-    {
-      asm volatile(boff);
-    }
-    else
-    {
-    shell();
+  while (1) {
+    beff = Nkhandler();
+    if (beff != '\n') {
+      boff += beff;
+    } else {
+      if (strcmp(boff, "exit") == 0) {
+        break;
+      } else {
+        asm volatile(boff);
+      }
+      boff = "";
     }
   }
 }
