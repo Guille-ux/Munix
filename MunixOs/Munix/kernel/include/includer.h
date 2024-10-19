49ee2aacd79b68ccf32dd90b82c39dccf4180797
Guille <guilleleiratemes@gmail.com>
2024-10-19 18:43:39 +0100

    all .h files are loaded by includer.h

---
+++ MunixOs/Munix/kernel/include/includer.h
@@ -0,0 +1,14 @@
+#include "io.h"
+#include "errors.h"
+#include "disk.h"
+#include "speaker.h"
+#include "timer.h"
+#include "ports.h"
+#include "types.h"
+#include "stnio.h"
+#include "ciphers.h"
+#include "math.h"
+#include "display.h"
+#include "shell.h"
+
+
--- MunixOs/Munix/kernel/kernel.c
+++ MunixOs/Munix/kernel/kernel.c
@@ -1,17 +1,6 @@
 #define BUFFER_SIZE 200
 
-#include "include/io.h"
-#include "include/errors.h"
-#include "include/disk.h"
-#include "include/timer.h"
-#include "include/speaker.h"
-#include "include/ports.h"
-#include "include/types.h"
-#include "include/stnio.h"
-#include "include/ciphers.h"
-#include "include/math.h"
-#include "include/display.h"
-#include "include/shell.h"
+#include "include/includer.h"
 
 void kernel_entry() {
     init_mat();
