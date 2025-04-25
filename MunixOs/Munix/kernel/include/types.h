1b9313f6e23f9f9304dae5daf9e5ca18f528cd8d
Guillermo leira temes <104579306+Guille-ux@users.noreply.github.com>
2025-04-25 06:33:10 +0200

    Update types.h

--- MunixOs/Munix/kernel/include/types.h
+++ MunixOs/Munix/kernel/include/types.h
@@ -28,10 +28,7 @@ typedef unsigned char Bool;
 // toca definir uint
 //-------------------------------------------------//
 
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef long long uint64_t;
+
 
 
 //oh, NULL
