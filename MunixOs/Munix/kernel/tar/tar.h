2f608e98761f50bed9518ec9b5c25f1559a04b4c
Guille-Ux <guillermoleiratemes@protonmail.com>
2026-01-21 15:24:44 +0000

    Creating files for tar

---
+++ MunixOs/Munix/kernel/tar/tar.h
@@ -0,0 +1,25 @@
+#ifndef MUNIX_TAR_H
+#define MUNIX_TAR_H
+
+#include <stdint.h>
+
+/*
+ *
+ * Definiciones para tar, tar lo usare para cosas como archivos de linker etc
+ * sera muy útil
+ *
+ */
+
+// la estructura salio de osdev
+typedef struct {
+	char name[100];
+	uint8_t mode[8];
+	uint8_t uid[8];
+	uint8_t gid[8];
+	uint8_t size[12];
+	uint8_t mtime[12];
+	uint8_t chksum[8];
+	uint8_t typeflag;
+} tar_header_t;
+
+#endif
