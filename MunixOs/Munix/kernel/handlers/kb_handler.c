60520fa651b24e60cf2c789add2c01afa8c36b01
Guille-Ux <guillermoleiratemes@protonmail.com>
2026-01-28 19:42:22 +0000

    adding scancode compress to make handling easier

--- MunixOs/Munix/kernel/handlers/kb_handler.c
+++ MunixOs/Munix/kernel/handlers/kb_handler.c
@@ -1,31 +1,45 @@
 #include "kb_handler.h"
 #include "../include/libcs2.h"
 
-uint8_t kb_ring_buffer[KB_BUFFER_SIZE];
+uint16_t kb_ring_buffer[KB_BUFFER_SIZE];
 size_t tail=0;
 size_t head=0;
 
+// ESTADOS INTERNOS
+char codebreak;
+char extended;
+
 const char ps2_kb_name[] = "MUNIX PS2 HANDLER";
 
-char buffer_pop() {
+uint16_t buffer_pop() {
 	if (tail == head) {
 		return '\0';
 	}
-	char ch = kb_ring_buffer[tail];
+	uint16_t ch = kb_ring_buffer[tail];
 	tail = (tail + 1) % KB_BUFFER_SIZE;
 	return ch;
 }
 
 void ps2_get_scancode() {
+	uint8_t scancode = inb(0x60); // leemos el puerto del teclado
+	if (scancode == 0xE0) extended=1; return;
+	if (scancode == 0xF0) codebreak=1; return;
+
+	uint16_t final_sc = scancode | (codebreak << 8) | (extended << 9);
+	buffer_push(final_sc);
+	extended=0;
+	codebreak=0;
+}
+
+void buffer_push(uint16_t item) {
 	if ((head + 1) % KB_BUFFER_SIZE == tail) {
 		return; // signifíca que esta lleno
 	}
-	char ch = inb(0x60);
-	kb_ring_buffer[head] = ch;
-	head = (head + 1) % KB_BUFFER_SIZE;
+	kb_ring_buffer[head] = item;
+	head = (head + 1) % KB_BUFFER_SIZE;	
 }
 
 void init_ps2_handler(kb_handler_t *kb_handler) {
 	kb_handler->name = ps2_kb_name;
-	kb_handler->push = ps2_get_scancode;
+	kb_handler->getch = ps2_get_scancode;
 }
--- MunixOs/Munix/kernel/handlers/kb_handler.h
+++ MunixOs/Munix/kernel/handlers/kb_handler.h
@@ -3,17 +3,18 @@
 
 #define KB_BUFFER_SIZE 512
 
-extern uint8_t kb_ring_buffer[KB_BUFFER_SIZE];
+extern uint16_t kb_ring_buffer[KB_BUFFER_SIZE];
 extern size_t tail;
 extern size_t head;
 
 typedef struct {
 	char *name;
-	void (*push)(void);
+	void (*getch)(void);
 } kb_handler_t;
 
-
-char buffer_pop();
+// nota, ahora son 16 bits para facilitarme la vida
+uint16_t buffer_pop();
+void buffer_push(uint16_t item);
 void ps2_get_scancode();
 void init_ps2_handler(kb_handler_t *kb_handler);
 
