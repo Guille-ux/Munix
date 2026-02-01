b6007de3a5579230b0455156aa0abf9d38b9cd5b
Guille-Ux <guillermoleiratemes@protonmail.com>
2026-02-01 12:02:16 +0000

    adding some def's

--- MunixOs/Munix/kernel/keyboard/keyboard.c
+++ MunixOs/Munix/kernel/keyboard/keyboard.c
@@ -2,8 +2,24 @@
 #include "../include/libcs2.h"
 #include "../handlers/kb_handler.h"
 
+bool bloq_mayus=false;
+bool bloq_num=false;
+bool shift=false;
+bool alt_gr=false;
+
+kb_entry_t *kb_layout;
+
+kb_entry_t kb_es_layout[128];
+
 void init_kb() {
 	// de momento dejamos solo la instalación del teclado ps2
 	init_ps2_handler(&ps2_handler);
 	kprintf("[ KEYBOARD HANDLER INSTALLED ]\n");
+}
+
+uint16_t kgetchar() {
+	uint16_t key = buffer_pop();
+	if (key & SPECIAL_KEY) {
+		return key;
+	}
 }
--- MunixOs/Munix/kernel/keyboard/keyboard.h
+++ MunixOs/Munix/kernel/keyboard/keyboard.h
@@ -2,14 +2,52 @@
 #define MUNIX_KEYBOARD_H
 
 #include "../handlers/kb_handler.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
 
 // inicialización del nuevo driver de teclado
 void init_kb();
+uint16_t kgetchar();
 
 // teclas
 #define SPECIAL_KEY (1 << 8)
+#define KEY_BREAK (1 << 7)
 
 #define KEY_HOME SPECIAL_KEY | 0x47
+#define KEY_APPS SPECIAL_KEY | 0x5D
+#define KEY_ESC 0x01
+#define KEY_BACKSPACE 0x0E
+#define KEY_TAB 0x0F
+#define KEY_ENTER 0x1C
+#define KEY_ALT_GR SPECIAL_KEY | 0x38
+#define KEY_BLOQ_MAYUS 0x3A
+#define KEY_BLOQ_NUM 0x45
+#define KEY_LEFT_SHIFT 0x2A
+#define KEY_LEFT_ALT 0x38
+#define KEY_RIGHT_SHIFT 0x36
 
+// teclas de función
+#define KEY_F1 0x3B
+#define KEY_F2 0x3C
+#define KEY_F3 0x3D
+#define KEY_F4 0x3E
+#define KEY_F5 0x3F
+#define KEY_F6 0x40
+#define KEY_F7 0x41
+#define KEY_F8 0x42
+#define KEY_F9 0x43
+#define KEY_F10 0x44
+#define KEY_F11 0x57
+#define KEY_F12 0x58
+
+typedef struct {
+	char normal;
+	char shift;
+	char alt;
+} kb_entry_t;
+
+extern kb_entry_t *kb_layout;
+extern kb_entry_t kb_es_layout[128];
 
 #endif
